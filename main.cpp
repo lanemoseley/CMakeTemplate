cda9da1db663345b247bde2a6b067b6c8d4616af
Lane Moseley <moseley.lane@gmail.com>
2023-05-12 12:50:42 +0000

    Add resources directory.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include <fstream>
 #include <stdio.h>
+#include <string>
 
 int main() {
-    printf("Hello, world!\n");
+    std::fstream greetingFile("resources/greeting.txt");
+    if (greetingFile.is_open()) {
+        std::string greeting;
+        getline(greetingFile, greeting);
+        printf("%s\n", greeting.c_str());
+    } else {
+        printf("ERROR: rdstate %d\n", greetingFile.rdstate());
+    }
 
     return 0;
 }
